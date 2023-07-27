e5e84b8843b93f936d0675b1fd9246d21e45f75d
Lawful831 <andregt455@gmail.com>
2023-07-27 17:32:26 -0600

    Questionable full functionality
    Audio and video buffering and recording done, synchronization? Nope, merging video and audio nope

--- clips/convert.py
+++ clips/convert.py
@@ -2,6 +2,29 @@ import os
 import sys
 import datetime
 import ffmpeg
+import subprocess
+
+def merge_video_and_audio(video_file, audio_file, output_file):
+    print("Start merging")
+    video_input = ffmpeg.input(video_file)
+    audio_input = ffmpeg.input(audio_file)
+    ffmpeg_command = [
+        "ffmpeg",
+        "-i", video_file,
+        "-i", audio_file,
+        "-af", "volume=5dB",
+        "-c:v", "copy",
+        "-c:a", "aac",
+        "-strict", "experimental",
+        output_file,
+    ]
+    try:
+    # Execute the ffmpeg command as a subprocess
+        subprocess.run(ffmpeg_command, check=True)
+        print("Normalization complete.")
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred: {e}")
+
 def delete_png_files(folder_path):
     try:
         for filename in os.listdir(folder_path):
@@ -12,6 +35,7 @@ def delete_png_files(folder_path):
         print("All PNG files deleted successfully.")
     except Exception as e:
         print(f"Error occurred: {e}")
+
 def create_video(framerate):
     # Get the current directory
     directory = os.path.dirname(os.path.abspath(__file__))
@@ -28,19 +52,16 @@ def create_video(framerate):
     # Create a video writer object
     clipname = datetime.datetime.utcnow().strftime("%m-%d-%Y-%H-%M-%S")
     print("Clip name: " + clipname)
+    voutput_file = os.path.join(directory, f'pre_{clipname}.mp4')
+    aoutput_file = os.path.join(directory, f'recorded.wav')
     output_file = os.path.join(directory, f'{clipname}.mp4')
 
     # Create an ffmpeg input object from the list of image files
-    input_args = [ffmpeg.input(os.path.join(directory, file),framerate=framerate) for file in image_files]
+    input_args = [ffmpeg.input(os.path.join(directory, file), framerate=framerate) for file in image_files]
 
     # Create the video and save it to the output file
-    for input_arg in input_args:
-        print(input_arg)
-    print("OUTPUT")
-    print(output_file)
     try:
-        ffmpeg.input(f'{directory}/%d.png', framerate=framerate).output(output_file, vcodec='libx264', r=framerate, pix_fmt='yuv420p').run()
-        #ffmpeg.output(*input_args, output_file,framerate=framerate,vcodec='libx264',r=framerate,pix_fmt='yuv420p').run(capture_stdout=True, capture_stderr=True)#give framerate if no work
+        ffmpeg.output(*input_args, voutput_file, vcodec='libx264', r=framerate, pix_fmt='yuv420p').overwrite_output().run()
     except ffmpeg.Error as e:
         print('stdout:', e.stdout.decode('utf8'))
         print('stderr:', e.stderr.decode('utf8'))
@@ -48,15 +69,17 @@ def create_video(framerate):
 
     print("Video writer done")
     print("Finished")
-    print(f"Video created: {output_file}")
+    print(f"Video created: {voutput_file}")
     print("Deleting png frames")
     delete_png_files(directory)
+    #merge_video_and_audio(voutput_file, aoutput_file, output_file)
+
 
 if __name__ == '__main__':
     # Check if the framerate argument is provided
     if len(sys.argv) != 2:
-       print("Usage: python script.py <framerate>")
-       sys.exit(1)
+        print("Usage: python script.py <framerate>")
+        sys.exit(1)
 
     try:
         framerate = int(sys.argv[1])
--- src/main.rs
+++ src/main.rs
@@ -1,125 +1,21 @@
-use std::fs;
-use std::io::Read;
+#[macro_use]
+extern crate log;
+use simplelog::*;
 use std::thread;
-use std::time::{Duration, Instant};
-use device_query::{DeviceState, DeviceQuery};
-use mki::{bind_key, Action, InhibitEvent, Keyboard, Sequence};
-use device_query::keymap::Keycode;
-use screenshots::Screen;
+mod lawfulvideo;
+mod audio;
+fn main() {
 
-use wasapi::*;
+    // Initialize the logger here if needed
 
-type Res<T> = Result<T, Box<dyn std::error::Error>>;
-
-struct CircularBuffer {
-    buffer: Vec<Vec<u8>>,
-    buffer_size: usize,
-    write_position: usize,
-    frame_rate: usize,
-    duration_secs: usize,
-}
-
-impl CircularBuffer {
-    fn new(frame_rate: usize, duration_secs: usize, frame_size: usize) -> CircularBuffer {
-        let buffer_size = duration_secs * frame_rate;
-        println!("My max capacity is: {}",buffer_size);
-        CircularBuffer {
-            buffer: vec![vec![0;frame_size]],
-            buffer_size,
-            write_position: 0,
-            frame_rate,
-            duration_secs,
-        }
-    }
-
-    fn write(&mut self, data: Vec<u8>) {
-        if self.buffer.len() < self.buffer_size {
-            self.buffer.push(data);
-        } else {
-            self.buffer[self.write_position] = data;
-            self.write_position = (self.write_position + 1) % self.buffer_size;
-        }
-    }
-
-    fn read_all(&self) -> Vec<Vec<u8>> {
-        // Filter out any empty frames from the buffer before returning
-        self.buffer.iter().filter(|frame| !frame.is_empty()).cloned().collect()
-    }
-
-}
-
-fn calculate_frame_size(screen: &Screen) -> usize {
-    let width = screen.display_info.width as usize;
-    let height = screen.display_info.height as usize;
-    let bytes_per_pixel = 4;
-    width * height*bytes_per_pixel
-}
-
-fn transform_frames_to_video(fps:usize){
-    let python_script_path = "clips/convert.py";
-    let fpstring = fps.to_string();
-    println!("Frames will be {}",fpstring);
-    let output = std::process::Command::new("cmd").
-                               arg("/C").
-                               arg("python").
-                               arg("clips/convert.py").
-                               arg(fpstring)
-                               .stdout(std::process::Stdio::piped())
-                               .spawn()
-                               .expect("Failed to convert");
-                    
-    if let Some(mut child_stdout) = output.stdout{
-        let mut output_data = String::new();
-        child_stdout
-            .read_to_string(&mut output_data)
-            .expect("Failed to read child process");
-        println!("{}",output_data);
-    }
-}
-
-fn main()->Res<()> {
-    let screens = Screen::all().unwrap();
-    let s = screens[0];
-    let framerate = 24;
-    let mut videobuffer = CircularBuffer::new(framerate, 3, calculate_frame_size(&s));
-    let device_state = DeviceState::new();
-    println!("Query? {:#?}", device_state.query_keymap());
-
-    initialize_mta();
-
-    loop {
-        
-        let keys: Vec<Keycode> = device_state.get_keys();
-        if keys.contains(&Keycode::Q) {
-            break;
-        }
-    
-        if mki::are_pressed(&[Keyboard::LeftAlt, Keyboard::X]) {
-            println!("Should start clipping");
-            let buffered_frames = videobuffer.read_all();
-            let mut count = 0;
-            let frame_delay = Duration::from_millis((1000 / framerate as u64) - 10); // -10ms to account for processing time
-            for frame in buffered_frames {
-                fs::write(format!("clips/{}.png", count), frame).unwrap();
-                count += 1;
-                println!("Count{}", count);
-                thread::sleep(frame_delay);
-            }
-            thread::spawn(move ||{transform_frames_to_video(framerate)});
-            println!("Clipped successfully");
-        }
-    
-        videobuffer.write(capture(&s));
-        println!("Buffer size: {}", videobuffer.read_all().len()); // Add this line to check buffer size
-        // Introduce a delay to control the frame rate
-        let frame_delay = Duration::from_millis((1000 / framerate as u64) - 10); // -10ms to account for processing time
-        thread::sleep(frame_delay);
-    }
-    Ok(())
-}
-
-fn capture(screen: &Screen) -> Vec<u8> {
-    let image = screen.capture().unwrap();
-    let buffer = image.buffer();
-    buffer.to_owned()
+    // Spawn two threads to execute the video and audio capture functions
+    let audio_thread = thread::spawn(|| {
+        audio::execute_audio_capture().unwrap();
+    });
+    let video_thread = thread::spawn(|| {
+        lawfulvideo::execute_video_capture().unwrap();
+    });
+    // Wait for the threads to finish capturing video and audio
+    //video_thread.join().expect("Video thread panicked!");
+    audio_thread.join().expect("Audio thread panicked!");
 }
